1a59108cdac191b201cd0f897199f08d974470be
Adam_Diass <adamrpdiass@hotmail.com>
2024-10-25 17:00:44 -0300

    feat: Classe Carro

---
+++ curso.php/Carro.php
@@ -0,0 +1,35 @@
+<?php
+class Carro{
+    public $marca;
+    public $modelo;
+    public $ano;
+    public $velocidadeAtual;
+
+    public function __construct($marca,$modelo,$ano)
+    {
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->ano = $ano;
+    }
+
+    public function acelerar($aumentarVelocidade){
+        $this->velocidadeAtual += $aumentarVelocidade;
+    }
+    public function frear($diminuirVelocidade){
+    $this->velocidadeAtual -= $diminuirVelocidade;
+}
+    public function exibirInformacoes(){
+        echo "Marca: {$this->marca}<br>Modelo: {$this->modelo}<br>Ano: {$this->ano}<br>Velocidade atual: {$this->velocidadeAtual} ";
+    }
+}
+
+$marca = "Chevrolet";
+$modelo = "Celta";
+$ano = 2016;
+$velocidadeAtual = 0;
+
+$carro1 = new Carro($marca,$modelo,$ano);
+
+$carro1->acelerar(100);
+$carro1->frear(50);
+$carro1->exibirInformacoes();
--- curso.php/ContaBancaria.php
+++ curso.php/ContaBancaria.php
@@ -11,10 +11,20 @@ class ContaBancaria{
     }
 
     public function depositar($valor){
-        $this->saldo += $valor; 
+        if($valor > 0){
+            $this->saldo += $valor; 
+        }
+        else{
+            echo "O valor deve ser positivo!" . "<br>";
+        }
+
     }
     public function sacar($valor){
+        if($valor > $this->saldo){
+            echo "Saldo insuficiente para saque!" . "<br>";
+        }else{
         $this->saldo -= $valor;
+    }
     }
     public function exibirSaldo(){
         print "Saldo: " . $this->saldo;
@@ -26,6 +36,6 @@ $titular = 'adam';
 $saldo = 1200;
 
 $conta1 = new ContaBancaria($titular,$saldo);
-$conta1->depositar(1200);
+$conta1->depositar(1600);
 $conta1->sacar(600);
 $conta1->exibirSaldo();
